92a6327e76030335f124c4bbd16e9225a047289e
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2021-01-01 09:14:47 +0900

    faker name

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,33 @@
-# User.create!(name:  "Example User",
-#              email: "example@railstutorial.org",
-#              password:              "foobar",
-#              password_confirmation: "foobar",
-#              admin: true)
+# メインのサンプルユーザーを1人作成する
+User.create!(name:  "OkayToyoji",
+             email: "okaytoyoji@co.jp",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin:     true,
+             activated: true,
+             activated_at: Time.zone.now)
+
+# ログイン用テストユーザーを1人作成する
+User.create!(name:  "testtest",
+             email: "test@co.jp",
+             password:              "testtest",
+             password_confirmation: "testtest",
+             activated: true,
+             activated_at: Time.zone.now)
 
 # 追加のユーザーをまとめて生成する
-# 99.times do |n|
-#   name  = Faker::Name.name
-#   email = "example-#{n+1}@railstutorial.org"
-#   password = "password"
-#   User.create!(name:  name,
-#                email: email,
-#                password:              password,
-#                password_confirmation: password)
-# end
+
+20.times do |n|
+  name  = Faker::JapaneseMedia::StudioGhibli.character
+  email = Faker::Internet.unique.email 
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password: password,
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
+end
+
+
+
