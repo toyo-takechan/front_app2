93ee5691f6929e122d973b5a877a024e816f0a9d
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2021-06-06 08:59:33 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'home#index'
+
+  root to: 'homes#index'
 
 end
--- test/controllers/homes_controller_test.rb
+++ test/controllers/homes_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-class HomeControllerTest < ActionDispatch::IntegrationTest
+class HomesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get home_index_url
+    get homes_index_url
     assert_response :success
   end
 
