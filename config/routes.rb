3361503c14c2b28322caff636b46a946aee9dac3
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2021-06-06 13:18:20 +0900

    heart chat

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
   root to: 'homes#index'
-
+  get '/homes/top', to: 'homes#top'
+  get '/homes/chat', to: 'homes#chat'
+  get '/homes/heart', to: 'homes#heart'
 end
