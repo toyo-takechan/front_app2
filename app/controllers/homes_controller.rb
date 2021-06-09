b52263ec9b00b1b507e1be75fa4a6acdcfa98373
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2021-06-09 13:18:07 +0900

    carpage1

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,13 @@
 class HomesController < ApplicationController
   def index
   end
+
+  def heart
+  end
+
+  def chat
+  end
+
+  def car
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root to: 'homes#index'
-  get '/homes/top', to: 'homes#top'
+  get '/homes/car', to: 'homes#car'
   get '/homes/chat', to: 'homes#chat'
   get '/homes/heart', to: 'homes#heart'
 end
