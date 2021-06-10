1d755bdf4b93f949d9bd13a4db11d37cf87b5362
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2021-06-11 02:07:53 +0900

    finish home2

--- Gemfile
+++ Gemfile
@@ -51,6 +51,11 @@ group :test do
   gem 'selenium-webdriver'
   # Easy installation and use of web drivers to run system tests with browsers
   gem 'webdrivers'
+  gem 'rails-controller-testing', '1.0.5'
+  gem 'minitest',                 '5.14.4'
+  gem 'minitest-reporters',       '1.3.8'
+  gem 'guard',                    '2.16.2'
+  gem 'guard-minitest',           '2.4.6'
 end
 
 
--- test/controllers/homes_controller_test.rb
+++ test/controllers/homes_controller_test.rb
@@ -1,9 +1,28 @@
 require 'test_helper'
 
 class HomesControllerTest < ActionDispatch::IntegrationTest
+
   test "should get index" do
-    get homes_index_url
+    get root_url
     assert_response :success
+    assert_select "title", "FrontApp2"
   end
 
+  test "should get car" do
+    get homes_car_url
+    assert_response :success
+    assert_select "title", "FrontApp2 /car"
+  end
+
+  test "shoud get chat" do
+    get homes_chat_url
+    assert_response :success
+    assert_select "title", "FrontApp2 /chat"
+  end
+
+  test "shoud get heart" do
+    get homes_heart_url
+    assert_response :success
+    assert_select "title", "FrontApp2 /heart"
+  end
 end
