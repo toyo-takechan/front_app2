fccd1e283a24f6a6da1e5d3c3696c7b6020f11d9
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2021-06-17 23:21:07 +0900

    carjquerytop

--- app/javascript/car.js
+++ app/javascript/car.js
@@ -6,7 +6,64 @@ $(function(){
 
   $('.car-drive').click(function(){
     // $('#cuple_man').show();
-    $('.home-car-big').show();
-  })
+    $('#couple_man').show();
+  });
+
+  $('#couple_man-modal').click(function(){
+    $('#couple_man').fadeOut();
+  });
+
+  $('.car_lover').click(function(){
+    // $('#cuple_man').show();
+    $('#lover_woman').show();
+  });
+
+  $('#lover_woman-modal').click(function(){
+    $('#lover_woman').fadeOut();
+  });
+
+  $('.car_shoubousya').click(function(){
+    $('#shoubousya_front').show();
+  });
+
+  $('#shoubousya_front-modal').click(function(){
+    $('#shoubousya_front').fadeOut();
+  });
+
+  $('.driving').click(function(){
+    $('#driving_red').show();
+  });
+
+  $('#driving_red-modal').click(function(){
+    $('#driving_red').fadeOut();
+  });
+
+  $('.f1').click(function(){
+    $('#f1_car').show();
+  });
+
+  $('#f1_car-modal').click(function(){
+    $('#f1_car').fadeOut();
+  });
+
+  $('.car_van_sougeisya').click(function(){
+    $('#car_van_sougeisya').show();
+  });
+
+  $('#car_van_sougeisya-modal').click(function(){
+    $('#car_van_sougeisya').fadeOut();
+  });
+
+  $('.unten_car').click(function(){
+    $('#unten_car').show();
+  });
+
+  $('#unten_car-modal').click(function(){
+    $('#unten_car').fadeOut();
+  });
+
+  
+
+
 
 });
