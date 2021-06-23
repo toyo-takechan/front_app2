f1f0cf3157685ecf728fbb32e777a71c9a03b2d2
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2021-06-23 13:19:24 +0900

    user new form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,24 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_url
+    else
+      render 'new'
+    end
   end
+  
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
@@ -16,4 +26,11 @@ class UsersController < ApplicationController
 
   def destroy
   end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email )
+    
+  end
+  
 end
