528c47e94cb37b1ada156f02b22ba302dfeecf19
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2021-06-29 10:36:48 +0900

    test home=>top

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
     get root_path
-    assert_template 'homes/index'
+    assert_template 'homes/top'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", car_path
     assert_select "a[href=?]", chat_path
