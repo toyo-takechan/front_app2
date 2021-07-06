da1e3f5c67db25d73af9fd8542569c6c86c4f304
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2021-07-06 23:35:43 +0900

    integrationtest sigunup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Front App　へようこそ!"
+      flash[:success] = "ユーザー登録に成功しました！　Front App　へようこそ!"
       redirect_to @user
     else
       render 'new'
@@ -26,6 +26,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to @user
+      
     else
       render 'edit'
     end
