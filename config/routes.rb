b4aac9ddbe60b8be12fdf1f5d376b45ca90d082e
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2021-07-10 00:54:35 +0900

    loginform

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
   # get 'users/show'
   # get 'users/new'
   # get 'users/create'
@@ -7,17 +8,20 @@ Rails.application.routes.draw do
   # get 'users/update'
   # get 'users/destroy'
   root to: 'homes#top'
-  get '/car',    to: 'homes#car'
-  get '/chat',   to: 'homes#chat'
-  get '/heart',  to: 'homes#heart'
-  get '/about',  to: 'homes#about'
-  get '/contact',to: 'homes#contact'
-  get '/signup' ,to: "users#new"
-  get '/robot1', to: "homes#robot1"
-  get '/robot2', to: "homes#robot2"
-  get '/robot3', to: "homes#robot3"
-  get '/robot4', to: "homes#robot4"
-
+  get    '/car',     to: 'homes#car'
+  get    '/chat',    to: 'homes#chat'
+  get    '/heart',   to: 'homes#heart'
+  get    '/about',   to: 'homes#about'
+  get    '/contact', to: 'homes#contact'
+  get    '/signup' , to: "users#new"
+  get    '/robot1',  to: "homes#robot1"
+  get    '/robot2',  to: "homes#robot2"
+  get    '/robot3',  to: "homes#robot3"
+  get    '/robot4',  to: "homes#robot4"
+  get    '/login',   to: "sessions#new"
+  post   '/login',   to: "sessions#create"
+  delete '/logout',  to: "sessions#destroy"
+  
   resources :users
 end
 
