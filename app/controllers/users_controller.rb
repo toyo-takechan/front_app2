2f0972749abf61365470926130aafb1da74fdc7b
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2021-07-23 10:53:46 +0900

    authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -26,6 +28,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
+      flash[:success] = "プロフィールを更新しました。"
       redirect_to @user
       
     else
@@ -42,5 +45,15 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password, :password_confirmation )
     
   end
+
+  # beforeアクション
+
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "ログインしてください。"
+        redirect_to login_url
+      end
+    end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ApplicationRecord
   validates :name,  presence: true, length: { maximum: 30 }
   VALID_EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, 
-                          format: { with: VALID_EMAIL_REGEX },
+                          format: { with: VALID_EMAIL_REGEX }, 
                           uniqueness: true
   has_secure_password 
-  validates :password, presence: true, length: { minimum: 6 }  
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true  
   
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
