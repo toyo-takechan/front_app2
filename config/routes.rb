38c1e31d4161fdedaf8b50af389e291ab6e9af71
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2021-07-30 20:38:43 +0900

    passwordresetscontroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get 'sessions/new'
   # get 'users/show'
   # get 'users/new'
@@ -25,5 +27,7 @@ Rails.application.routes.draw do
   
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+  
 end
 
