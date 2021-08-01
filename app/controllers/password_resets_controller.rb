ae9f7e52e6d2eb13c93331500a65e072346e4978
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2021-08-01 15:33:05 +0900

    passwordresetform

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,9 +1,11 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user,   only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
   def new
   end
 
   def create
-    @user = User.find_by(email: paramas[:password_resets][:email]j.downcase)
+    @user = User.find_by(email: paramas[:password_resets][:email].downcase)
     if @user
       @user.create_reset_digest
       @user.send_password_reset_email
@@ -17,4 +19,17 @@ class PasswordResetsController < ApplicationController
 
   def edit
   end
+
+  private
+
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    # 正しいユーザーかどうか確認する
+    def valid_user
+      unless(@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
+      redirect_to root_url
+      end
+    end
 end
