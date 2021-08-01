5ff79a6658420f7828435df2f145ba6c1918baad
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2021-08-01 13:04:49 +0900

    password_resets_controller.rb

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,6 +2,19 @@ class PasswordResetsController < ApplicationController
   def new
   end
 
+  def create
+    @user = User.find_by(email: paramas[:password_resets][:email]j.downcase)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = "パスワードの再設定用のメールを送りました。"
+      redirect_to root_url
+    else
+      flash_now[:danger] = "Emailが見つかりませんでした。"
+      render 'new'
+    end
+  end
+
   def edit
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_28_003227) do
+ActiveRecord::Schema.define(version: 2021_08_01_033744) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2021_07_28_003227) do
     t.datetime "reset_sent_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "reset_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
