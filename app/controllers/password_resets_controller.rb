3d658019d6ba93c4bee256cb5a15baa089eba6a9
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2021-08-01 18:59:26 +0900

    finish password-reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,18 +1,20 @@
 class PasswordResetsController < ApplicationController
   before_action :get_user,   only: [:edit, :update]
   before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
+
   def new
   end
 
   def create
-    @user = User.find_by(email: paramas[:password_resets][:email].downcase)
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
     if @user
       @user.create_reset_digest
       @user.send_password_reset_email
       flash[:info] = "パスワードの再設定用のメールを送りました。"
       redirect_to root_url
     else
-      flash_now[:danger] = "Emailが見つかりませんでした。"
+      flash.now[:danger] = "Emailが見つかりませんでした。"
       render 'new'
     end
   end
@@ -20,7 +22,24 @@ class PasswordResetsController < ApplicationController
   def edit
   end
 
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, :blank)
+      render 'edit'
+    elsif @user.update(user_params)
+      log_in @user
+      flash[:success] = "パスワードのリセットに成功しました。"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
+
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
 
     def get_user
       @user = User.find_by(email: params[:email])
@@ -30,6 +49,14 @@ class PasswordResetsController < ApplicationController
     def valid_user
       unless(@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
       redirect_to root_url
+      end
+    end
+
+    # トークンが期限切れかどうか確認する
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] = "パスワードのリセットが期限切れになりました。"
+        redirect_to new_password_reset_url
       end
     end
 end
